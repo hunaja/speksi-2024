1ed80a758f7968949bf259ee2782628fdd4d63d6
elias <79339968+hunaja@users.noreply.github.com>
2024-03-06 15:05:27 +0200

    markon nimi

--- app/page.tsx
+++ app/page.tsx
@@ -496,7 +496,7 @@ export default function Home() {
                   <div className="flex-1 h-full flex flex-col justify-center place-items-center">
                     <Image src={logistiikka} alt="Logistiikka" className="rounded-2xl m-auto" />
                     <h2 className="text-xl drop-shadow-2xl text-teal-600 my-10">Logistiikka</h2>
-                    <p>-</p>
+                    <p>Marko Varvikko</p>
                   </div>
                 </div>
                 <div className="flex flex-col items-center h-full justify-content-center place-content-center justify-center">
